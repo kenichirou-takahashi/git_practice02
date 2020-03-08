42305b65a2a6e19bdceb40da76038e24666e76a5
takahashikenichirou <new.ton.notwen19@gmail.com>
2020-03-08 18:45:16 +0900

    3,5,15の倍数対応fizzbuzz

--- fizzbazz.py
+++ fizzbazz.py
@@ -1,6 +1,13 @@
 def fizzbuzz_convert(number):
+    if number % 15 == 0:
+        return 'fizzbuzz'
+
     if number % 3 == 0:
         return 'fizz'
+    if number % 5 == 0:
+        return 'buzz'
 
 
 assert fizzbuzz_convert(3) == 'fizz'
+assert fizzbuzz_convert(5) == 'buzz'
+assert fizzbuzz_convert(15) == 'fizzbuzz'
